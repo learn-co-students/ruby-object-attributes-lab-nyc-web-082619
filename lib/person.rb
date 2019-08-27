f856f170d08ffc949ad3c8c200635adcae661816
Eugenia Kim <elk303@nyu.edu>
2019-08-27 14:27:35 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person 
+  def name= (newname)
+    @name = newname
+  end
+  
+  def name
+    @name 
+  end
+  
+  def job= (newjob)
+    @job = newjob
+  end
+  
+  def job
+    @job 
+  end
+end
